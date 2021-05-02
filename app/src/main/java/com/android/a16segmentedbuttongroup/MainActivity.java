1836cdec65a3c47308f49ea9b348a5fe5a36999b
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-02 14:03:08 +0700

    Segmented button group

--- app/src/main/java/com/android/a16segmentedbuttongroup/MainActivity.java
+++ app/src/main/java/com/android/a16segmentedbuttongroup/MainActivity.java
@@ -3,6 +3,9 @@ package com.android.a16segmentedbuttongroup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import co.ceryle.segmentedbutton.SegmentedButtonGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SegmentedButtonGroup segmentedButtonGroup = (SegmentedButtonGroup) findViewById(R.id.segmentedButtonGroup);
+        segmentedButtonGroup.setOnClickedButtonPosition(new SegmentedButtonGroup.OnClickedButtonPosition() {
+            @Override
+            public void onClickedButtonPosition(int position) {
+                if (position==0){
+                    Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
+                }else if (position==1){
+                    Toast.makeText(MainActivity.this, "Register", Toast.LENGTH_SHORT).show();
+
+                }else if (position==2){
+                    Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
